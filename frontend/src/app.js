f8c5e17618acc661a6b725b5a149f35c24ff9dd5
xbeatriz <beatrizcostadoc@gmail.com>
2025-05-14 22:12:16 +0100

    Organização dos documentos

---
+++ frontend/src/app.js
@@ -0,0 +1,12 @@
+// Check if the browser supports service workers
+if ('serviceWorker' in navigator) {
+
+  // Try to register the service worker file
+  navigator.serviceWorker.register('service-worker.js')
+
+    // If successful, log a confirmation message in the console
+    .then(() => console.log('Service Worker registered'))
+
+    // If there's an error, log it in the console
+    .catch(err => console.error('SW registration failed', err));
+}
